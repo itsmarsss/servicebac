2345fd00d72ada01fdceaf592189d6b2a66cbedd
itsmarsss <itsmarzzzzzz@gmail.com>
2024-02-11 09:31:03 -0500

    Add built files to backend `build`

--- back-end/server.js
+++ back-end/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger);
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "build")));
 
 app.set("view engine", "ejs");
 
@@ -28,5 +30,10 @@ function logger(req, res, next) {
 
 app.use("/api/user", userRouter);
 app.use("/api/partner", partnerRouter);
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 
 app.listen(3000);
