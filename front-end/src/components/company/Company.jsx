2bcc89049dcbefd63f3503b7e03bb5719ed062a7
itsmarsss <itsmarzzzzzz@gmail.com>
2024-02-11 02:33:24 -0500

    Add edit modal

--- front-end/src/components/company/Company.jsx
+++ front-end/src/components/company/Company.jsx
@@ -1,5 +1,6 @@
 import "./Company.css";
 import React from "react";
+import EditModal from "../../components/editModal/EditModal.jsx";
 import { useState, useEffect } from "react";
 import Cookies from "js-cookie";
 
@@ -8,11 +9,16 @@ function Company() {
   const [editing, setEditing] = useState(false);
   const [ownedServices, setOwnedServices] = useState([]);
 
+  const [serviceId, setServiceId] = useState();
+  const [serviceName, setServiceName] = useState("");
+  const [category, setCategory] = useState("");
+  const [data, setData] = useState("");
+
   const getToken = () => {
     return Cookies.get("token") || "";
   };
 
-  useEffect(() => {
+  const getOwnedServices = () => {
     try {
       setLoading(true);
       fetch("http://localhost:3000/api/partner/owned-services", {
@@ -33,15 +39,96 @@ function Company() {
     } catch (error) {
       console.error("Error getting dashboard:", error);
     }
+  };
+
+  const showModal = (serviceId) => {
+    try {
+      setLoading(true);
+      fetch(`http://localhost:3000/api/partner/id/${serviceId}`, {
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${getToken()}`,
+        },
+      })
+        .then((data) => data.json())
+        .then((response) => {
+          if (response.success) {
+            const service = response.service;
+            console.log("Fetched service:", service);
+            setServiceId(service.serviceId);
+            setServiceName(service.serviceName);
+            setCategory(service.category);
+
+            let data = service.data;
+
+            if (typeof data !== "string") {
+              try {
+                data = JSON.stringify(service.data, null, "\t");
+              } catch (err) {}
+            }
+
+            setData(data);
+            setEditing(true);
+          } else {
+            alert(response.message);
+          }
+          setLoading(false);
+          setEditing(true);
+        });
+    } catch (error) {
+      console.error("Error getting service:", error);
+    }
+  };
+
+  const saveService = () => {
+    setEditing(false);
+    try {
+      setLoading(true);
+      let serviceData = data;
+      try {
+        serviceData = JSON.parse(data);
+      } catch (err) {}
+
+      fetch("http://localhost:3000/api/partner/update-service", {
+        method: "PUT",
+        headers: {
+          authorization: `Bearer ${getToken()}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          serviceId: serviceId,
+          serviceName: serviceName,
+          category: category,
+          data:
+            typeof serviceData === "object" ? { ...serviceData } : serviceData,
+        }),
+      })
+        .then((data) => data.json())
+        .then((response) => {
+          if (!response.success) {
+            alert(response.message);
+          }
+          setLoading(false);
+        });
+    } catch (error) {
+      console.error("Error getting service:", error);
+    }
+  };
+
+  useEffect(() => {
+    getOwnedServices();
   }, []);
 
   return (
     <>
       <div className="company">
         <div className="title">Dashboard:</div>
+        {loading && (
+          <div className="loader">
+            <div className="loader_text">Loading...</div>
+          </div>
+        )}
         <div className="owned_services">
-          {loading && <h1>Loading...</h1>}
-
           {ownedServices.length > 0 && (
             <>
               {ownedServices.map((service) => (
@@ -52,17 +139,29 @@ function Company() {
                       <div className="service_category">{service.category}</div>
                       <div className="service_id">ID: {service.serviceId}</div>
                     </div>
-                    <button>Edit</button>
+                    <button onClick={() => showModal(service.serviceId)}>
+                      Edit
+                    </button>
                   </div>
                 </div>
               ))}
-              <button className="add_service" onClick={() => setEditing(true)}>
-                Add New Service
-              </button>
+              <button className="add_service">Add New Service</button>
             </>
           )}
         </div>
-        {editing && <div className="edit_modal"></div>}
+        {editing && (
+          <EditModal
+            saveService={saveService}
+            setEditing={setEditing}
+            setServiceName={setServiceName}
+            setCategory={setCategory}
+            setData={setData}
+            serviceId={serviceId}
+            serviceName={serviceName}
+            category={category}
+            data={data}
+          />
+        )}
       </div>
     </>
   );
---
+++ front-end/src/components/editModal/EditModal.jsx
@@ -0,0 +1,71 @@
+import "./EditModal.css";
+import React from "react";
+
+function EditModal({
+  saveService,
+  setEditing,
+  setServiceName,
+  setCategory,
+  setData,
+  serviceId,
+  serviceName,
+  category,
+  data,
+}) {
+  return (
+    <div className="edit_modal">
+      <div className="edit">
+        <div className="edit_id">Editing: {serviceId}</div>
+        <div className="edit_property">
+          <label>
+            Service Name:{" "}
+            <span className="property_note">(100 characters)</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Service Name"
+            value={serviceName}
+            onChange={(e) => setServiceName(e.target.value)}
+            maxLength={100}
+          />
+        </div>
+        <div className="edit_property">
+          <label>
+            Category: <span className="property_note">(50 characters)</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            maxLength={50}
+          />
+        </div>
+        <div className="edit_property edit_data_cont">
+          <label>
+            Data:{" "}
+            <span className="property_note">
+              (Text-only or JSON-only, 10,000 characters)
+            </span>
+          </label>
+          <textarea
+            className="edit_data"
+            type="text"
+            placeholder="Text and links"
+            value={data}
+            onChange={(e) => setData(e.target.value)}
+            maxLength={10000}
+          />
+        </div>
+        <div className="edit_actions">
+          <button className="fill_button" onClick={() => saveService()}>
+            Save
+          </button>
+          <button onClick={() => setEditing(false)}>Cancel</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EditModal;
