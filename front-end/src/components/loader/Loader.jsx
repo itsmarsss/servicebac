25ed5d308f74a3288d144d2572dadb046bbd6214
itsmarsss <itsmarzzzzzz@gmail.com>
2024-02-11 03:17:22 -0500

    Add `Loader` component

---
+++ front-end/src/components/loader/Loader.jsx
@@ -0,0 +1,12 @@
+import "./Loader.css";
+import React from "react";
+
+function Loader() {
+  return (
+    <div className="loader">
+      <div className="loader_text">Loading...</div>
+    </div>
+  );
+}
+
+export default Loader;
